4847dc244cd46d0c5053165110be012b39e25a1f
AlexPresso <blond.alexandre@hotmail.com>
2022-05-03 15:24:42 +0200

    Implement interactions

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,7 +3,7 @@ const EmbedUtils = require('../utils/EmbedUtils');
 module.exports = async (client, member) => {
     const embed = EmbedUtils.messageWithTitleAndAuthor(
         "",
-        `${member} a rejoint le serveur.`,
+        `${member} a rejoint le serveur (${member.guild.memberCount} membres).`,
         member
     ).setColor(EmbedUtils.colorGreen);
 
