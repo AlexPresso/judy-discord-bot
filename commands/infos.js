687c94d8118fbe28095ce53294140d1ccd7d63da
AlexPresso <15526962+AlexPresso@users.noreply.github.com>
2023-03-30 11:52:34 +0200

    Update infos command

--- commands/infos.js
+++ commands/infos.js
@@ -8,7 +8,10 @@ module.exports = {
         const embed = EmbedUtils.defaultEmbed()
             .setThumbnail(client.user.avatarURL())
             .setTitle("Infos")
-            .setDescription(`${client.user.username} a été développée par <@168436075058954240> en utilisant la librairie [Discord.js](https://discord.js.org)`)
+            .setDescription(`
+                ${client.user.username} a été développée par <@168436075058954240> en utilisant la librairie [Discord.js](https://discord.js.org).\n
+                Son code est open-source et disponible [ici](https://github.com/AlexPresso/judy-discord-bot).
+            `)
 
         interaction.reply({embeds: [embed]});
     }
