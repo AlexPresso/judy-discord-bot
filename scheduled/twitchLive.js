b7cebc0097839ecaa0def0f71ae3add654e05067
AlexPresso <15526962+AlexPresso@users.noreply.github.com>
2024-01-09 02:26:38 +0100

    Continue twitchLive.js

--- scheduled/twitchLive.js
+++ scheduled/twitchLive.js
@@ -1,11 +1,11 @@
 const Twitch = require('../utils/TwitchAPI');
+const EmbedUtils = require('../utils/EmbedUtils');
 const {GuildScheduledEventEntityType, GuildScheduledEventPrivacyLevel} = require("discord-api-types/v10");
 
 module.exports = {
-    schedule: "* * * * * *",
+    schedule: "0 * * * * *",
     task: async client => {
         const config = client.config.twitch || {};
-
         if(!config.channel)
             return;
 
@@ -18,48 +18,67 @@ module.exports = {
             return;
 
         const newState = response.data.data[0];
-        const oldState = client._state.twitchPrevState;
+        const oldState = client._state.twitch.prevState;
+        const manager = client.guilds.resolve(client.config.bot.guildId)?.scheduledEvents;
+        const previousEvent = await getOrFetchPreviousEvent(client, manager);
 
         if(!oldState && newState) {
-            liveStart(client, newState);
+            startOrEditLiveEvent(client, newState, manager, previousEvent);
+            notifyLiveStart(client, newState);
         } else if(oldState && !newState) {
-            liveStop(client, oldState);
+            stopLiveEvent(client, oldState);
         } else {
-            liveStop(client, oldState);
-            liveStart(client, newState);
+            startOrEditLiveEvent(client, newState, manager, previousEvent);
         }
 
-        client._state.prevStreamState = newState;
+        client._state.twitch.prevState = newState;
     }
 }
 
-async function liveStart(client, state) {
+async function notifyLiveStart(client, state) {
+    client.channels.resolve(client.config.twitch.discordChannel)?.send({
+        message: `@everyone https://twitch.tv/${client.config.twitch.channel}`
+    });
+}
+
+async function startOrEditLiveEvent(client, state, manager, previousEvent) {
     const date = new Date();
     const image = await Twitch.getThumbnail(state.thumbnail_url, 800, 400);
 
-    client.guilds.resolve(client.config.bot.guildId)?.scheduledEvents.create({
+    const options = {
         name: client.config.twitch.message.replaceAll("%userName%", "Alex'Presso"),
         description: state.title,
         image: Buffer.from(image.data, "utf-8"),
-        scheduledStartTime: date.setSeconds(date.getSeconds() + 2),
         scheduledEndTime: date.setDate(date.getDate() + 1),
         privacyLevel: GuildScheduledEventPrivacyLevel.GuildOnly,
         entityType: GuildScheduledEventEntityType.External,
         entityMetadata: {
             location: `https://twitch.tv/${client.config.twitch.channel}`
         }
-    });
+    };
+
+    client._state.twitch.scheduledEvent = await previousEvent ?
+        manager.edit(previousEvent, options) :
+        manager.create({
+            scheduledStartTime: date.setSeconds(date.getSeconds() + 2),
+            ...options
+        });
 }
 
-async function liveStop(client, state) {
-    const manager = client.guilds.resolve(client.config.bot.guildId)?.scheduledEvents;
-    const scheduledEvents = await manager.fetch();
+async function stopLiveEvent(client, manager, previousEvent) {
+    if(!previousEvent)
+        return;
 
-    for(const [id, event] of scheduledEvents) {
-        if(event.entityMetadata?.location?.includes("https://twitch.tv")) {
-            manager.delete(event);
-            client._state.twitchPrevState = null;
-            return;
-        }
+    manager.delete(previousEvent);
+    client._state.twitchPrevState = null;
+}
+
+async function getOrFetchPreviousEvent(client, manager) {
+    if(client._state.twitch.scheduledEvent)
+        return client._state.twitch.scheduledEvent;
+
+    for(const [_, event] of await manager.fetch()) {
+        if(event.entityMetadata?.location?.includes("https://twitch.tv"))
+            return event;
     }
 }
