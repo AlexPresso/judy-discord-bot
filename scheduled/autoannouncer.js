191685ee8ee6ab7fbdbb713816b028d990a53f5e
AlexPresso <15526962+AlexPresso@users.noreply.github.com>
2025-09-06 00:38:07 +0200

    update dependencies and add autoannouncer

--- scheduled/autoannouncer.js
+++ scheduled/autoannouncer.js
@@ -4,16 +4,16 @@ module.exports = {
         const announces = client.persistentData.autoannouncer || [],
             now = new Date();
 
-        for (const announce in announces) {
+        for (const announce of announces) {
             const cronTime = announce.cronTime;
 
             try {
                 const [minutes, hours, , , dayOfWeek] = cronTime.split(' ');
 
                 if (
-                    parseInt(minutes, 10) === cronTime.getMinutes() &&
-                    parseInt(hours, 10) === cronTime.getHours() &&
-                    parseInt(dayOfWeek, 10) === cronTime.getDate()
+                    parseInt(minutes, 10) === now.getMinutes() &&
+                    parseInt(hours, 10) === now.getHours() &&
+                    parseInt(dayOfWeek, 10) === now.getDate()
                 ) {
                     client.channels.cache.get(announce.channelId)?.send(announce.message);
                 }
